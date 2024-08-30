3f278db0897f12b78937259bbf377cc4fee508a0
Valou <anonanonymous018@gmail.com>
2024-08-30 11:35:18 +0200

    make an interactible sketch

--- scripts.js
+++ scripts.js
@@ -1,21 +1,46 @@
 let aqua = 'graduated';
+
 let div = document.createElement('div');
 div.classList.add('child-div');
-let hr = document.createElement('hr');
+
+let lineContainer = document.createElement('div');
+lineContainer.classList.add('line-div');
+
 let containerDiv = document.querySelector('.container-div');
 
-for (let i=0; i<255; i++) {
-   // if (i % 15 === 0) {
-   //     containerDiv.appendChild(hr.cloneNode(true));
-   // }
-    containerDiv.appendChild(div.cloneNode(true));
+let numberOfLines = 16;
+
+function remakeTheHtml () {
+    containerDiv.innerHTML = '';
+    lineContainer.innerHTML= '';
+    for (let i=0; i<numberOfLines; i++) {
+        lineContainer.appendChild(div.cloneNode(true));
+    }
+
+    for (let i=0; i<numberOfLines; i++) {
+        containerDiv.appendChild(lineContainer.cloneNode(true));
+    }
 }
+remakeTheHtml()
 
-let containerDivAll = document.querySelectorAll('.container-div')
 
+let containerDivAll = document.querySelectorAll('.child-div')
+console.log(containerDivAll);
 containerDivAll.forEach(() => {
     addEventListener('mouseover', (e) => {
         let target = e.target
-        console.log(target)
+        target.classList.add('new-display')
     })
+    addEventListener('mouseout', (e) => {
+        let target = e.target
+        target.classList.remove('new-display')
+    })
+})
+
+let gridButton = document.querySelector('.grid-button');
+
+gridButton.addEventListener('click', () => {
+    let inputGrid = prompt('Please choose the divs per line, up to 100');
+    numberOfLines = inputGrid;
+    remakeTheHtml();
 })
