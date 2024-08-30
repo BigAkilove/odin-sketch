1e086b9b06c348cf6e6eb4254b3cb1a838dcc6e9
Valou <anonanonymous018@gmail.com>
2024-08-30 14:04:46 +0200

    remove the hover effect on other divs

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let aqua = 'graduated';
 
-let div = document.createElement('div');
-div.classList.add('child-div');
+let squareDiv = document.createElement('div');
+squareDiv.classList.add('child-div');
 
 let lineContainer = document.createElement('div');
 lineContainer.classList.add('line-div');
@@ -14,28 +14,47 @@ function remakeTheHtml () {
     containerDiv.innerHTML = '';
     lineContainer.innerHTML= '';
     for (let i=0; i<numberOfLines; i++) {
-        lineContainer.appendChild(div.cloneNode(true));
+        lineContainer.appendChild(squareDiv.cloneNode(true));
     }
 
     for (let i=0; i<numberOfLines; i++) {
         containerDiv.appendChild(lineContainer.cloneNode(true));
     }
 }
-remakeTheHtml()
+remakeTheHtml();
 
+document.addEventListener('mouseover', (e) => {
+    let target = e.target;
 
-let containerDivAll = document.querySelectorAll('.child-div')
-console.log(containerDivAll);
+    switch(target.className) {
+        case 'child-div':
+            target.classList.add('new-display');
+            break;
+    }
+        
+});
+document.addEventListener('mouseout', (e) => {
+    let target = e.target;
+
+    switch(target.className) {
+        case 'child-div new-display':
+            target.classList.remove('new-display');
+            break;
+    }
+ });
+
+/* let containerDivAll = document.querySelectorAll('.child-div')
+
 containerDivAll.forEach(() => {
     addEventListener('mouseover', (e) => {
-        let target = e.target
-        target.classList.add('new-display')
-    })
+        let target = e.target;
+        target.classList.add('new-display');
+    });
     addEventListener('mouseout', (e) => {
-        let target = e.target
-        target.classList.remove('new-display')
-    })
-})
+        let target = e.target;
+        target.classList.remove('new-display');
+    });
+}); */
 
 let gridButton = document.querySelector('.grid-button');
 
